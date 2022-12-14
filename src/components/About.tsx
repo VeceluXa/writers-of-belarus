787b1ed64d278a44186d229f44699ca6f49d79a9
Chernika_56 <bosko_2004@mail.ru>
2022-12-15 00:55:30 +0300

    About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,12 +5,17 @@ export function About() {
     return(
         <Container
             sx={{
-                marginBottom: 5
+                marginBottom: 55
             }}
         >
             <Typography
-                variant="h2"
+                variant="h4"
+                align="center"
+                color="text.primary"
+                gutterBottom 
                 sx={{
+                    fontStyle: 'oblique', 
+                    fontWeight: 'medium',
                     textAlign: 'center'
                 }}
             >
@@ -18,13 +23,19 @@ export function About() {
             </Typography>
 
             <Typography
-                variant="body1"
+                variant="h6" 
+                align="center" 
+                color="text.secondary" 
+                paragraph
                 sx={{
-                    textAlign: 'justify'
+                    fontWeight: 'light',
+                    fontStyle: 'italic',
+                    mx: 10,
+                    textAlign: 'center'
                 }}
             >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, corrupti tempora? Voluptatum accusamus in laudantium sequi quos soluta eum voluptas excepturi vitae iure qui quam, nulla dolorem corporis? Laudantium maiores commodi, sapiente dolores ut odio magnam nulla placeat ipsa praesentium voluptates id fuga optio sint eveniet nihil eligendi quaerat quod beatae provident. Ipsum quae vel officia ipsam hic corporis non dignissimos, doloremque eum rem ducimus quibusdam, vitae, ullam quos. Ratione labore officia, qui velit maxime in dolorum sapiente hic. Voluptatum enim adipisci ut exercitationem libero cum culpa recusandae, dolorem officia ea non aperiam! Et ab reprehenderit asperiores assumenda, molestias nobis!
-            </Typography>
+                Our website will help you immerse yourself in the world of Belarusian literature. We have collected for you more than three hundred of the most famous Belarusian authors.
+                </Typography>
         </Container>
     )
 }
