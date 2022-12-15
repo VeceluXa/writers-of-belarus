331b8f865ebdb8c8b9d210f1004ada98866ec36b
Chernika_56 <bosko_2004@mail.ru>
2022-12-16 00:24:39 +0300

    Loader 2.0

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,25 +1,44 @@
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
 
 export default function App() {
   return (
     <Box>
       <CircularLoading />
+      <Content />
     </Box>
   );
 }
 
 const CircularLoading = () => (
-  <>
-    <CircularProgress
-      size={70}
-      sx={{
-        position: "fixed",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-        zIndex: 2
-      }}
-    />
-  </>
-);
+    <>
+      <CircularProgress
+        size={70}
+        sx={{
+          position: "fixed",
+          left: "50%",
+          top: "40%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 2,
+          color: "text.secondary"
+        }}
+      />
+    </>
+  );
+
+
+const Content = () => (
+    <Box sx={{ p: 1 }}>
+      <Typography 
+        color="text.secondary"
+        variant="h4"
+        align="center"
+        mt={50}
+        ml={12}
+      >
+        Loading...
+      </Typography>
+      
+    </Box>
+  );
